28e6cbcba5d3da488ffff7cc6f9f8d577dc10d95
Gian <gian.mars71@gmail.com>
2021-07-26 17:20:12 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+  
+package MarioBrickBreaker;
+
+
+import javax.swing.JFrame;
+import java.applet.Applet;
+
+public class Main extends Applet{
+	public static void main(String []args) {
+		JFrame obj = new JFrame();
+		
+		Mainplay gamePlay = new Mainplay();
+		obj.setBounds(10,10,700,600);
+		
+		obj.setTitle("MarioYoshi ");
+		obj.setResizable(false);
+		obj.setVisible(true);
+                  
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                System.out.println("BRICKS WORKING");
+		obj.add(gamePlay);
+		
+	}
+
+
+}
